b98b8934b06bad9e45dc9ca8042e4899a3cf797c
Dorian Cottle <thenewdorian21@gmail.com>
2024-11-13 23:22:20 -0700

    please work

--- obj/Debug/net8.0/Advanced-Frontend-Final-Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Advanced-Frontend-Final-Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advanced-Frontend-Final-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae5ec5ae3325f1955d0b53aa2e4f7fba458be098")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1533bdcd5a80a62024f1baeb4af46fe190daef83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advanced-Frontend-Final-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Advanced-Frontend-Final-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
