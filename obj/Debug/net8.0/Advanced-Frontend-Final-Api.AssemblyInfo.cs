995378586531cd7f4b72c59d863f6e72bf4f8da3
Dorian Cottle <thenewdorian21@gmail.com>
2024-11-13 22:28:14 -0700

    trying same host

--- obj/Debug/net8.0/Advanced-Frontend-Final-Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Advanced-Frontend-Final-Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Advanced-Frontend-Final-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82ac33ddd365c8eaedbacad57216d7a243ddcb76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30a9ed86e97b5ac09487e06da87cd1e08556740c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Advanced-Frontend-Final-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Advanced-Frontend-Final-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
